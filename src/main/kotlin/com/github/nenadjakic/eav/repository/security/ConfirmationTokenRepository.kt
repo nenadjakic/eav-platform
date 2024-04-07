44a571e4a70bd5b2e1c1ede90fe2dcef8ad9f944
nenadjakic <nenad.jakic@gmail.com>
2024-04-07 21:30:58 +0200

    Improved data retrieval from the database using Entity graph. Updated README.md

--- src/main/kotlin/com/github/nenadjakic/eav/repository/security/ConfirmationTokenRepository.kt
+++ src/main/kotlin/com/github/nenadjakic/eav/repository/security/ConfirmationTokenRepository.kt
@@ -1,9 +1,11 @@
 package com.github.nenadjakic.eav.repository.security
 
 import com.github.nenadjakic.eav.entity.security.ConfirmationToken
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.Optional
 
 interface ConfirmationTokenRepository : JpaRepository<ConfirmationToken, Long> {
+    @EntityGraph(attributePaths = ["user"])
     fun findByToken(token: String): Optional<ConfirmationToken>
 }
--- src/main/kotlin/com/github/nenadjakic/eav/repository/security/RefreshTokenRepository.kt
+++ src/main/kotlin/com/github/nenadjakic/eav/repository/security/RefreshTokenRepository.kt
@@ -1,9 +1,11 @@
 package com.github.nenadjakic.eav.repository.security
 
 import com.github.nenadjakic.eav.entity.security.RefreshToken
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.OffsetDateTime
 
 interface RefreshTokenRepository : JpaRepository<RefreshToken, Long> {
+    @EntityGraph(attributePaths = ["user"])
     fun findByUserUsernameAndTokenAndExpireAtGreaterThanEqual(username: String, token: String, now: OffsetDateTime): RefreshToken?
 }
