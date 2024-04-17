53ea5e1ac52e79900ea97a15a4421bb6569ba73d
nenadjakic <nenad.jakic@gmail.com>
2024-04-17 21:55:47 +0200

    Optimized data retrieval using JPA.

--- src/main/kotlin/com/github/nenadjakic/eav/repository/EntityRepository.kt
+++ src/main/kotlin/com/github/nenadjakic/eav/repository/EntityRepository.kt
@@ -1,6 +1,15 @@
 package com.github.nenadjakic.eav.repository
 
 import com.github.nenadjakic.eav.entity.Entity
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface EntityRepository : JpaRepository<Entity, Long>
+interface EntityRepository : JpaRepository<Entity, Long> {
+    @EntityGraph(attributePaths = ["entityType"])
+    override fun findAll(): List<Entity>
+
+    @EntityGraph(attributePaths = ["entityType"])
+    override fun findAll(pageable: Pageable): Page<Entity>
+}
