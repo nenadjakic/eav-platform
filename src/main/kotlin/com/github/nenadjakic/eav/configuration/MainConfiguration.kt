0ed09fcc03aea2ae65bb2c1d9eecdac4fe2013b6
nenadjakic <nenad.jakic@gmail.com>
2024-04-20 22:55:35 +0200

    Included authentication scheme definition in the OpenAPI specification.

--- src/main/kotlin/com/github/nenadjakic/eav/configuration/MainConfiguration.kt
+++ src/main/kotlin/com/github/nenadjakic/eav/configuration/MainConfiguration.kt
@@ -1,7 +1,13 @@
 package com.github.nenadjakic.eav.configuration
 
 import com.github.nenadjakic.eav.dto.converter.*
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.modelmapper.ModelMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
@@ -21,6 +27,29 @@ open class MainConfiguration(
     private val userDetailsService: UserDetailsService,
     private val passwordEncoder: PasswordEncoder
 ) {
+
+    private fun createAPIKeyScheme(): SecurityScheme {
+        return SecurityScheme().type(SecurityScheme.Type.HTTP)
+            .bearerFormat("JWT")
+            .scheme("bearer")
+    }
+
+    @Bean
+    fun openAPI(@Value("\${application.name}") name: String?, @Value("\${application.version}") version: String?): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title(name)
+                    .version(version)
+            )
+            .addSecurityItem(SecurityRequirement().addList("Bearer_Authentication"))
+            .components(Components().addSecuritySchemes("Bearer_Authentication", createAPIKeyScheme()))
+    }
+
+    @Bean
+    open fun cacheManager() : CacheManager {
+        return ConcurrentMapCacheManager("roles", "attributePermissions")
+    }
 
     @Bean
     open fun modelMapper(): ModelMapper {
