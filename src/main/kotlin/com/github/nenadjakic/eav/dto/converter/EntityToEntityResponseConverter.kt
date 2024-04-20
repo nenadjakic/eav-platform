48fbb47d8ac07b067b8292d67246bd01811de56d
nenadjakic <nenad.jakic@gmail.com>
2024-04-20 15:14:03 +0200

    Changed entity response model.

--- src/main/kotlin/com/github/nenadjakic/eav/dto/converter/EntityToEntityResponseConverter.kt
+++ src/main/kotlin/com/github/nenadjakic/eav/dto/converter/EntityToEntityResponseConverter.kt
@@ -1,5 +1,7 @@
 package com.github.nenadjakic.eav.dto.converter
 
+import com.github.nenadjakic.eav.dto.AttributeSimpleResponse
+import com.github.nenadjakic.eav.dto.AttributeValueSimpleResponse
 import com.github.nenadjakic.eav.dto.EntityResponse
 import com.github.nenadjakic.eav.dto.EntityTypeSimpleResponse
 import com.github.nenadjakic.eav.entity.Entity
@@ -7,13 +9,19 @@ import org.modelmapper.AbstractConverter
 
 class EntityToEntityResponseConverter : AbstractConverter<Entity, EntityResponse>() {
     override fun convert(source: Entity?): EntityResponse {
+        val attibuteValues: MutableList<AttributeValueSimpleResponse> = mutableListOf()
+        source!!.attributeValues.forEach { attibuteValues.add(AttributeValueSimpleResponse(
+            AttributeSimpleResponse(it.entityAttributeId.attribute.id!!, it.entityAttributeId.attribute.name),
+            it.value
+        )) }
         return EntityResponse(
-            source!!.id!!,
+            source.id!!,
             EntityTypeSimpleResponse(
                 source.entityType.id!!,
                 source.entityType.name,
                 source.entityType.description
-            )
+            ),
+            attibuteValues
         )
     }
 }
